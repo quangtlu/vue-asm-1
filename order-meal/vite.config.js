a8c808005ba5740382cd1f9c2c11bc4438675c0e
Quang Duong <quang.duong@pirago.work>
2024-02-28 23:23:15 +0700

    add

--- order-meal/vite.config.js
+++ order-meal/vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://quangtlu.github.io/vue-asm-1/',
+  base: '/vue-asm-1/',
 });
